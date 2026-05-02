669dfc165ce06bcf6eef77a347e1b87740bc8197
YueZhengZhiYu <3194626511@qq.com>
2026-05-02 16:17:56 +0800

    UART

---
+++ UART/UART/Core/Src/usart.c
@@ -0,0 +1,259 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file    usart.c
+  * @brief   This file provides code for the configuration
+  *          of the USART instances.
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2026 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "usart.h"
+
+/* USER CODE BEGIN 0 */
+#include <stdio.h>
+#include <stdarg.h>
+
+uint8_t Serial_RxData;		//串口接收数据
+uint8_t Serial_RxFlag;		//串口接收标志位
+/* USER CODE END 0 */
+
+UART_HandleTypeDef huart1;
+
+/* USART1 init function */
+
+void MX_USART1_UART_Init(void)
+{
+
+  /* USER CODE BEGIN USART1_Init 0 */
+
+  /* USER CODE END USART1_Init 0 */
+
+  /* USER CODE BEGIN USART1_Init 1 */
+
+  /* USER CODE END USART1_Init 1 */
+  huart1.Instance = USART1;
+  huart1.Init.BaudRate = 9600;
+  huart1.Init.WordLength = UART_WORDLENGTH_8B;
+  huart1.Init.StopBits = UART_STOPBITS_1;
+  huart1.Init.Parity = UART_PARITY_NONE;
+  huart1.Init.Mode = UART_MODE_TX_RX;
+  huart1.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  huart1.Init.OverSampling = UART_OVERSAMPLING_16;
+  if (HAL_UART_Init(&huart1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN USART1_Init 2 */
+
+  /* USER CODE END USART1_Init 2 */
+
+}
+
+void HAL_UART_MspInit(UART_HandleTypeDef* uartHandle)
+{
+
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+  if(uartHandle->Instance==USART1)
+  {
+  /* USER CODE BEGIN USART1_MspInit 0 */
+
+  /* USER CODE END USART1_MspInit 0 */
+    /* USART1 clock enable */
+    __HAL_RCC_USART1_CLK_ENABLE();
+
+    __HAL_RCC_GPIOA_CLK_ENABLE();
+    /**USART1 GPIO Configuration
+    PA9     ------> USART1_TX
+    PA10     ------> USART1_RX
+    */
+    GPIO_InitStruct.Pin = TTL_TX_Pin;
+    GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
+    HAL_GPIO_Init(TTL_TX_GPIO_Port, &GPIO_InitStruct);
+
+    GPIO_InitStruct.Pin = TTL_RX_Pin;
+    GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    HAL_GPIO_Init(TTL_RX_GPIO_Port, &GPIO_InitStruct);
+
+    /* USART1 interrupt Init */
+    HAL_NVIC_SetPriority(USART1_IRQn, 0, 0);
+    HAL_NVIC_EnableIRQ(USART1_IRQn);
+  /* USER CODE BEGIN USART1_MspInit 1 */
+
+  /* USER CODE END USART1_MspInit 1 */
+  }
+}
+
+void HAL_UART_MspDeInit(UART_HandleTypeDef* uartHandle)
+{
+
+  if(uartHandle->Instance==USART1)
+  {
+  /* USER CODE BEGIN USART1_MspDeInit 0 */
+
+  /* USER CODE END USART1_MspDeInit 0 */
+    /* Peripheral clock disable */
+    __HAL_RCC_USART1_CLK_DISABLE();
+
+    /**USART1 GPIO Configuration
+    PA9     ------> USART1_TX
+    PA10     ------> USART1_RX
+    */
+    HAL_GPIO_DeInit(GPIOA, TTL_TX_Pin|TTL_RX_Pin);
+
+    /* USART1 interrupt Deinit */
+    HAL_NVIC_DisableIRQ(USART1_IRQn);
+  /* USER CODE BEGIN USART1_MspDeInit 1 */
+
+  /* USER CODE END USART1_MspDeInit 1 */
+  }
+}
+
+/* USER CODE BEGIN 1 */
+
+/**
+  * 函    数：串口发送一个字节
+  * 参    数：Byte 要发送的一个字节
+  * 返 回 值：无
+  */
+void Serial_SendByte(uint8_t Byte)
+{
+	HAL_UART_Transmit(&huart1, &Byte, 1, 100);	//HAL库串口发送单字节
+}
+
+/**
+  * 函    数：串口发送一个数组
+  * 参    数：Array 要发送数组的首地址
+  * 参    数：Length 要发送数组的长度
+  * 返 回 值：无
+  */
+void Serial_SendArray(uint8_t *Array, uint16_t Length)
+{
+	uint16_t i;
+	for (i = 0; i < Length; i ++)		//遍历数组
+	{
+		Serial_SendByte(Array[i]);		//依次调用Serial_SendByte发送每个字节数据
+	}
+}
+
+/**
+  * 函    数：串口发送一个字符串
+  * 参    数：String 要发送字符串的首地址
+  * 返 回 值：无
+  */
+void Serial_SendString(char *String)
+{
+	uint8_t i;
+	for (i = 0; String[i] != '\0'; i ++)//遍历字符数组（字符串），遇到字符串结束标志位后停止
+	{
+		Serial_SendByte(String[i]);		//依次调用Serial_SendByte发送每个字节数据
+	}
+}
+
+/**
+  * 函    数：次方函数（内部使用）
+  * 返 回 值：返回值等于X的Y次方
+  */
+uint32_t Serial_Pow(uint32_t X, uint32_t Y)
+{
+	uint32_t Result = 1;	//设置结果初值为1
+	while (Y --)			//执行Y次
+	{
+		Result *= X;		//将X累乘到结果
+	}
+	return Result;
+}
+
+/**
+  * 函    数：串口发送数字
+  * 参    数：Number 要发送的数字，范围：0~4294967295
+  * 参    数：Length 要发送数字的长度，范围：0~10
+  * 返 回 值：无
+  */
+void Serial_SendNumber(uint32_t Number, uint8_t Length)
+{
+	uint8_t i;
+	for (i = 0; i < Length; i ++)		//根据数字长度遍历数字的每一位
+	{
+		Serial_SendByte(Number / Serial_Pow(10, Length - i - 1) % 10 + '0');	//依次调用Serial_SendByte发送每位数字
+	}
+}
+
+/**
+  * 函    数：使用printf需要重定向的底层函数
+  * 参    数：保持原始格式即可，无需变动
+  * 返 回 值：保持原始格式即可，无需变动
+  */
+int fputc(int ch, FILE *f)
+{
+	Serial_SendByte(ch);			//将printf的底层重定向到自己的发送字节函数
+	return ch;
+}
+
+/**
+  * 函    数：自己封装的prinf函数
+  * 参    数：format 格式化字符串
+  * 参    数：... 可变的参数列表
+  * 返 回 值：无
+  */
+void Serial_Printf(char *format, ...)
+{
+	char String[100];				//定义字符数组
+	va_list arg;					//定义可变参数列表数据类型的变量arg
+	va_start(arg, format);			//从format开始，接收参数列表到arg变量
+	vsprintf(String, format, arg);	//使用vsprintf打印格式化字符串和参数列表到字符数组中
+	va_end(arg);					//结束变量arg
+	Serial_SendString(String);		//串口发送字符数组（字符串）
+}
+
+/**
+  * 函    数：获取串口接收标志位
+  * 参    数：无
+  * 返 回 值：串口接收标志位，范围：0~1，接收到数据后，标志位置1，读取后标志位自动清零
+  */
+uint8_t Serial_GetRxFlag(void)
+{
+	if (Serial_RxFlag == 1)			//如果标志位为1
+	{
+		Serial_RxFlag = 0;
+		return 1;					//则返回1，并自动清零标志位
+	}
+	return 0;						//如果标志位为0，则返回0
+}
+
+/**
+  * 函    数：获取串口接收的数据
+  * 参    数：无
+  * 返 回 值：接收的数据，范围：0~255
+  */
+uint8_t Serial_GetRxData(void)
+{
+	return Serial_RxData;			//返回接收的数据变量
+}
+
+/**
+  * 函    数：USART1中断回调函数
+  * 参    数：huart 串口句柄
+  * 返 回 值：无
+  * 注意事项：此函数为HAL库中断回调函数，中断触发后自动执行
+  */
+/*
+
+
+*/
+
+/* USER CODE END 1 */
+
+/* USER CODE END 1 */
